0992fc48df77a4b163acdb0fce4ace9b100dc4d4
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-30 16:47:45 -0600

    add third feature test

---
+++ spec/features/visitor_can_add_item_to_cart_spec.rb
@@ -0,0 +1,71 @@
+require "rails_helper"
+
+RSpec.feature "When a visitor" do
+  context "visits the items page and clicks add to cart" do
+    it "the items are in the cart" do
+      item1 = create(:item)
+
+      visit '/items'
+      click_on "Add to Cart"
+      click_on "View Cart"
+      expect(current_path).to eq('/cart')
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.price)
+      expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
+      expect(page).to have_content(item1.price)
+
+      visit '/items'
+      click_on "Add to Cart"
+      click_on "View Cart"
+      expect(page).to have_content(item1.price*2)
+        
+    end
+  end
+
+  context "visits the category page and clicks add to cart" do
+    it "the items are in the cart" do
+      item1 = create(:item)
+
+      visit "/#{item.category}"
+      click_on "Add to Cart"
+      click_on "View Cart"
+      
+      expect(current_path).to eq('/cart')
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.price)
+      expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
+      expect(page).to have_content(item1.price)
+
+      visit "/#{item.category}"
+      click_on "Add to Cart"
+      click_on "View Cart"
+      expect(page).to have_content(item1.price * 2)
+        
+    end
+  end
+
+  context "visits the category page and clicks add to cart" do
+    it "the items are in the cart" do
+      item1 = create(:item)
+
+      visit item_path(item1)
+      click_on "Add to Cart"
+      click_on "View Cart"
+      expect(current_path).to eq('/cart')
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.price)
+      expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
+      expect(page).to have_content(item1.price)
+
+      visit item_path(item1)
+      click_on "Add to Cart"
+      click_on "View Cart"
+      expect(page).to have_content(item1.price * 2)
+        
+    end
+  end
+end
+
--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -2,21 +2,21 @@ require "rails_helper"
 
 RSpec.feature "When a visitor" do
   context "visits the items page" do
-    item1, item2, item3 = create_list(:items)
+    item1, item2, item3 = create_list(:item)
 
     visit "/items"
     expect(page).to have_content(item1.title)
     expect(page).to have_content(item1.description)
     expect(page).to have_content(item1.price)
-    # expect(page).to have_xpath("//img[@src=#{item1.image_path}")
+    expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
     expect(page).to have_content(item2.title)
     expect(page).to have_content(item2.description)
     expect(page).to have_content(item2.price)
-    # expect(page).to have_xpath("//img[@src=#{item2.image_path}")
+    expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
     expect(page).to have_content(item3.title)
     expect(page).to have_content(item3.description)
     expect(page).to have_content(item3.price)
-    # expect(page).to have_xpath("//img[@src=#{item3.image_path}")
+    expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
     expect(page).to have_content("Login")
   end
 end
