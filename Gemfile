985c76ec0203fce28067ec297e09a5d98bbc5b82
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-30 16:02:20 -0600

    create first two feature tests

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -51,6 +52,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari'
 gem 'haml'
 gem 'erb2haml', group: :development
+
 group :development, :test do
   gem 'rspec-rails'
   gem 'launchy'
---
+++ spec/features/visitor_can_browse_item_by_category_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.feature "When a visitor" do
+  context "visits a category" do
+    it "sees all items in that category" do
+      category1 = create(:category_with_items, item_count: 2)
+      item1 = category1.items.first
+      item2 = category1.items.last
+      category2 = create(:category_with_items, item_count: 2)
+      item3 = category2.items.first
+      item4 = category2.items.last
+      
+
+      visit "/#{category1.title}"
+      expect(find('.page-heading')).to have_content(category1.title)
+      expect(page).to have_content(item1.title)
+      expect(page).to have_content(item2.title)
+      expect(page).to_not have_content(item3.title)
+      expect(page).to_not have_content(item4.title)
+
+      visit "/#{category2.title}"
+      expect(find('.page-heading')).to have_content(category2.title)
+      expect(page).to have_content(item3.title)
+      expect(page).to have_content(item4.title)
+      expect(page).to_not have_content(item1.title)
+      expect(page).to_not have_content(item2.title)
+    end
+  end
+end
+
