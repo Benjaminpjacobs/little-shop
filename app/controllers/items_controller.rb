4a438e7fbc7125ba87fb4563574248a4be529235
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-05-31 14:27:47 -0600

    Troubleshooting paperclip

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,6 @@
 class ItemsController < ApplicationController
-  
+  def index
+    @items = Item.all
+  end
+
 end
--- spec/features/visitors/visitor_can_view_items_spec.rb
+++ spec/features/visitors/visitor_can_view_items_spec.rb
@@ -3,20 +3,20 @@ require "rails_helper"
 RSpec.feature "When a visitor" do
   it "visits the items page" do
     item1, item2, item3 = create_list(:item, 3)
-
     visit items_path
-    expect(page).to have_content(item1.title)
+    save_and_open_page
+    expect(page).to have_content(item1.name)
     expect(page).to have_content(item1.description)
     expect(page).to have_content(item1.price)
-    expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
-    expect(page).to have_content(item2.title)
+    expect(page).to have_xpath("//img[@src='spec/fixtures/images/gear.png'")
+    expect(page).to have_content(item2.name)
     expect(page).to have_content(item2.description)
     expect(page).to have_content(item2.price)
-    expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
-    expect(page).to have_content(item3.title)
+    expect(page).to have_xpath("//img[@src='spec/fixtures/images/gear.png'")
+    expect(page).to have_content(item3.name)
     expect(page).to have_content(item3.description)
     expect(page).to have_content(item3.price)
-    expect(page).to have_xpath("//img[@src='assets/image/gear.png'")
+    expect(page).to have_xpath("//img[@src='spec/fixtures/images/gear.png'")
     expect(page).to have_content("Login")
   end
 end
