93a7d36ee5db67a6693aa20c48731f7c3a3f5489
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-05-31 14:39:51 -0600

    Take out Xpath expectations in visitor_can_view_item_spec

--- spec/features/visitors/visitor_can_view_items_spec.rb
+++ spec/features/visitors/visitor_can_view_items_spec.rb
@@ -4,19 +4,17 @@ RSpec.feature "When a visitor" do
   it "visits the items page" do
     item1, item2, item3 = create_list(:item, 3)
     visit items_path
-    save_and_open_page
+
     expect(page).to have_content(item1.name)
     expect(page).to have_content(item1.description)
     expect(page).to have_content(item1.price)
-    expect(page).to have_xpath("//img[@src='spec/fixtures/images/gear.png'")
     expect(page).to have_content(item2.name)
     expect(page).to have_content(item2.description)
     expect(page).to have_content(item2.price)
-    expect(page).to have_xpath("//img[@src='spec/fixtures/images/gear.png'")
     expect(page).to have_content(item3.name)
     expect(page).to have_content(item3.description)
     expect(page).to have_content(item3.price)
-    expect(page).to have_xpath("//img[@src='spec/fixtures/images/gear.png'")
+    expect(page.all("img").count).to eq(3)
     expect(page).to have_content("Login")
   end
 end
