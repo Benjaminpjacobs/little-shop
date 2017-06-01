0610adb13496a29b4b64a77138ac06289772a39f
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-01 10:44:18 -0600

    create sessions controller and root directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  root to: "root_directory#index"
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :items, only: [:index]
   resources :categories, only: [:show]
 end
