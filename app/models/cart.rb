c46003e99d7976881e76bc0c63efd04c90dcc512
Sarah Kirk <kirk710@gmail.com>
2017-06-01 12:10:15 -0600

    adds total price method to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -16,4 +16,14 @@ class Cart
   def quantity_for(item_id)
     contents[item_id.to_s]
   end
+
+  def total_price
+    sum = 0
+    # .reduce(0) do |sum, (item_id, quantity)|
+    @contents.each do |item_id, quantity|
+      item = Item.find(item_id)
+      sum += item.price*quantity
+    end
+    sum
+  end
 end
---
+++ spec/features/visitors/visitor_can_add_multiple_unique_item_to_cart_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "When a visitor" do
+  context "visits the items page and clicks add to cart on two items" do
+    it "the unique items are in the cart" do
+      item1 = create(:item)
+      item2 = create(:item)
+      total = item1.price + item2.price
+      visit item_path(item1)
+      click_on "Add to Cart"
+
+      visit item_path(item2)
+      click_on "Add to Cart"
+
+      click_on "View Cart"
+      expect(current_path).to eq(carts_path)
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.price)
+      expect(page.all("img").count).to eq(2)
+
+      expect(page).to have_content(item2.name)
+      expect(page).to have_content(item2.description)
+      expect(page).to have_content(item2.price)
+      expect(page).to have_content(total)
+    end
+  end
+end
