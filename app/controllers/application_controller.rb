18a8b5b34fd4df52fd48ca7791496208164c3df8
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-01 13:21:21 -0600

    fixes user/guest partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :load_cart
   
   helper_method :current_user
+  helper_method :current_guest
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
+  def current_guest
+    if current_user
+      current_user.role
+    else
+      "guest"
+    end
+  end
+  
   private
 
   def load_cart
--- spec/features/users/registered_user_can_login_spec.rb
+++ spec/features/users/registered_user_can_login_spec.rb
@@ -1,16 +1,21 @@
 require "rails_helper"
 
 RSpec.feature "As registered user" do
+  before(:all) do 
+    reset_session!
+  end
+  
   it "can login" do
-
+    
     user = create(:user)
 
     visit root_path
     click_on "Login"
     fill_in "Email", with: user.email
     fill_in "Password", with: "password"
-
     click_button "Login"
+    save_and_open_page
+
     expect(page).to have_content("Logged in as #{user.first_name}")
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Logout")
