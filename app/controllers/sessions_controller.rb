62641e62b4aabad87177464546f30da1a269b8a7
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-01 10:58:38 -0600

    create sessions#new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
+  end
+
+  def create
 
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: "root_directory#index"
+  root to: "items#index"
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   resources :items, only: [:index]
   resources :categories, only: [:show]
 end
