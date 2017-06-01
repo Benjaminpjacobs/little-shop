c59ecca01d45e18037a4a12d2af9126a94c01c5b
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-01 13:54:37 -0600

    create dashboard controller dashboard index view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  resources :dashboard, only: [:index]
+
   resources :items, only: [:index, :show]
 
   resources :categories, only: [:show]
