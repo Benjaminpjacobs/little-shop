5879215d551cdbb261e62b333f5b5ec39ca01836
Sarah Kirk <kirk710@gmail.com>
2017-06-01 13:26:09 -0600

    changed the file path in delete item from cart test

--- spec/features/visitors/visitor_can_remove_items_from_cart_spec.rb
+++ spec/features/visitors/visitor_can_remove_items_from_cart_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.feature "When a visitor" do
   context "visits the cart" do
-    it "he or she can delete an item" do
+    it "they can delete an item" do
       item1 = create(:item)
       cart = Cart.new
       cart.add_item(item1.id)
 
       visit cart_path
       click_on "Remove"
-      expect(current_path).to eq('/cart')
+      expect(current_path).to eq(carts_path)
       expect(page).to have_content("Successfully removed #{item1.name} from your cart")
       #capybara test color
       expect(page).to have_link(item_path(item1))
@@ -17,4 +17,3 @@ RSpec.feature "When a visitor" do
     end
   end
 end
-
