9fad83d7e5786118db3e7698d045fb4755ca1d2f
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-02 13:22:48 -0600

    Merge conflict resolution

--- app/controllers/carts/items_controller.rb
+++ app/controllers/carts/items_controller.rb
@@ -1,14 +1,10 @@
 class Carts::ItemsController < ApplicationController
-  def create
-    item_id = params[:item_id]
-    item = Item.find(item_id)
-    if params[:perform] == "remove"
-      @cart.remove_item(item_id)
-      flash[:remove_item_from_cart] = "Successfully removed #{view_context.link_to(item.name, item_path(item))} from your cart."
-    else
-      @cart.contents[item_id] = params[:quantity].to_i
-      flash[:updated_cart] = "Successfully updated #{view_context.link_to(item.name, item_path(item))} quantity in your cart."
-    end
-    redirect_to carts_path
+
+  def edit
+    flash[:updated_cart] = "Successfully updated #{view_context.link_to(item.name, item_path(item))} quantity in your cart."
+  end
+
+  def destroy
+    flash[:remove_item_from_cart] = "Successfully removed #{view_context.link_to(item.name, item_path(item))} from your cart."
   end
 end
