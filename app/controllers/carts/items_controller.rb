d9b1d3afae41b9daee5feaeadf5c044044585282
Charlie PS Corrigan <charlie.ps.corrigan@gmail.com>
2017-06-02 13:25:31 -0600

    Update items_controller.rb

--- app/controllers/carts/items_controller.rb
+++ app/controllers/carts/items_controller.rb
@@ -1,10 +1,14 @@
 class Carts::ItemsController < ApplicationController
 
   def edit
+    @cart.contents[params[:item_id]]= params[:quantity].to_i
     flash[:updated_cart] = "Successfully updated #{view_context.link_to(item.name, item_path(item))} quantity in your cart."
+    redirect_to carts_path
   end
 
   def destroy
+    @cart.remove_item(params[:item_id])
     flash[:remove_item_from_cart] = "Successfully removed #{view_context.link_to(item.name, item_path(item))} from your cart."
+    redirect_to carts_path
   end
 end
