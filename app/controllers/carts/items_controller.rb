effcc961bf78aabe0dec9f298c97b9c9c22e4d82
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-02 13:14:54 -0600

    Add flash messages to cart

--- app/controllers/carts/items_controller.rb
+++ app/controllers/carts/items_controller.rb
@@ -1,8 +1,10 @@
 class Carts::ItemsController < ApplicationController
   def create
     item_id = params[:item_id]
+    item = Item.find(item_id)
     if params[:perform] == "remove"
       @cart.remove_item(item_id)
+      flash[:remove_item_from_cart] = "Successfully removed #{view_context.link_to(item.name, item_path(item))} from your cart."
     else
       @cart.contents[item_id] = params[:quantity].to_i
     end
