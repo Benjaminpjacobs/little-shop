c3ea895cdf4115771a8da6e79dbd02aefd86d72e
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-03 10:02:20 -0600

    confirms aunthenticated cannot view another user's data

--- spec/features/authorization/authenticated_user_can_only_see_authorized_data_spec.rb
+++ spec/features/authorization/authenticated_user_can_only_see_authorized_data_spec.rb
@@ -5,23 +5,24 @@ RSpec.feature "As an authenticated user" do
     reset_session!
   end
   
-  xit "cannot view another user's data" do
-    user1 = create(:user_with_orders, order_count: 1)
+  it "cannot view another user's data" do
+    user1 = create(:user_with_orders)
     order1 = user1.orders.first
-    user2 = create(:user_with_orders, order_count: 1)
+    user2 = create(:user_with_orders)
     order2 = user2.orders.first
+
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
 
-    visit orders_path
+    visit user_orders_path(user1)
 
-    expect(page).to_not have_content(order2.created_at)
-    expect(page).to have_content(order1.created_at)
+    expect(page).to_not have_content(order2.total)
+    expect(page).to have_content(order1.total)
 
     visit dashboard_index_path
-    
-    expect(page).to_not have_content(user2.first_name)
-    expect(page).to have_content(user1.first_name)
+
+    expect(page).to_not have_content(user2.last_name)
+    expect(page).to have_content(user1.last_name)
   end
 
   xit "cannot view the admin screens or perform admin functions" do
