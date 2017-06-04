8d5c8eb9a04c5669bb4478c859b55f5165385b4a
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-04 09:52:39 -0600

    adds retired partial to item index page

--- spec/features/users/user_cannot_add_retired_item_to_cart_spec.rb
+++ spec/features/users/user_cannot_add_retired_item_to_cart_spec.rb
@@ -3,13 +3,16 @@ require "rails_helper"
 RSpec.feature "As a user" do
   context "when viewing a retired item" do
     it "cannot add to cart" do
-      user = user(:create)
-      item = item(:create, status: 1)
+      user = create(:user)
+      item = create(:item, status: 1)
 
       visit item_path(item)
-
       expect(page).to_not have_content("Add to Cart")
-      expect(page).to have_content("Item Retired")
+      expect(page).to have_button("Item Retired", disabled: true)
+      
+      visit items_path(item)
+      expect(page).to_not have_content("Add to Cart")
+      expect(page).to have_button("Item Retired", disabled: true)
     end
   end
 end
