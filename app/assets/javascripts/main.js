3165176934dcde7fd192a23062bcecdf8cd273ca
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-04 16:46:37 -0600

    complete show/hide jquery for admin dashboard

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   $(".order-filter a").click(function(){
-   $("#all-orders").find("." + this.id).toggle()
+   $("#all-orders").find("." + this.id).addClass("show")
+   $("#all-orders").find("." + this.id).removeClass("hidden")
+   $("#all-orders .orders-table").not("." + this.id).removeClass("show")
+   $("#all-orders .orders-table").not("." + this.id).addClass("hidden")
  })
+
+  $("#show-all").click(function(){
+    $("#all-orders .orders-table").removeClass("hidden")
+  })
 });
