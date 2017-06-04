74b7656007239b7370085e02e626b0122790a79c
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-04 10:21:00 -0600

    adds model tests for order item

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe OrderItem do
     it {should belong_to(:order)}
     it {should belong_to(:item)}
     it {should validate_presence_of(:qty)}
+    it {should validate_presence_of(:line_item_subtotal)}
   end
   
   it "should calculate line item subtotal" do
+    order = create(:order)
+    item = create(:item)
+    
+    order_item = order.order_items.create(item_id: item.id, order_id: order.id)
+
+    expect(order_item.line_item_subtotal).to eq(item.price)
+
+    order_item = order.order_items.create(item_id: item.id, order_id: order.id, qty: 2)
+
+    expect(order_item.line_item_subtotal).to eq(item.price * 2)
   end
 
 end
