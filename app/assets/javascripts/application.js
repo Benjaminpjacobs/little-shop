01f9eb2a97ff596b0093f21247aa79825db66901
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-04 16:17:05 -0600

    setup jquery-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require main
 //= require_tree .
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $(".order-filter a").click(function(){
    $("#all-orders").find("." + this.id).toggle()
-  }
+ })
 });
