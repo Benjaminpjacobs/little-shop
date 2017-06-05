da73733e2d6d88caf7bcbb8b0cfbec3fc29a7476
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-05 12:51:10 -0600

    troubleshooting images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,15 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.paperclip_defaults = {
+    default_url: => '/public/assets/gear.png'
     storage: :s3,
-    
     s3_credentials: {
+      s3_host_name: "s3-us-west-2.amazonaws.com",
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-      s3_protocol: 'https'
+      s3_protocol: "https"
     }
   }
 
