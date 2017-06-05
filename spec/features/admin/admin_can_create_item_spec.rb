7587443466ae666adaa6864cc9329fcf00e445ab
Sarah Kirk <kirk710@gmail.com>
2017-06-05 17:14:47 -0600

    gets second test to pass

--- spec/features/admin/admin_can_create_item_spec.rb
+++ spec/features/admin/admin_can_create_item_spec.rb
@@ -25,29 +25,29 @@ RSpec.describe "As an admin" do
     expect(page.all("img").count).to eq(2)
   end
 
-#   xit "can creates an item with default photo" do
-#     admin = create(:user, role: 1)
-#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_respond(admin)
-#     cat1, cat2 = create_list(:category, 2)
-#     item = build(:item)
+  it "can creates an item with default photo" do
+    admin = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    cat1, cat2 = create_list(:category, 2)
+    item = build(:item)
 
-#     visit new_admin_item_path
+    visit new_admin_item_path
 
-#     fill_in "Name", with: item.name
-#     fill_in "Description", with: item.description
-#     fill_in "Price", with: item.price
-#     select cat1.title, from: "Categories"
-#     select cat2.title, from: "Categories"
-#     click_on "Create Item"
+    fill_in "Name", with: item.name
+    fill_in "Description", with: item.description
+    fill_in "Price", with: item.price
+    select cat1.title
+    select cat2.title
+    click_on "Create Item"
 
-#     expect(page).to have_content(item.name)
-#     expect(page).to have_content(item.description)
-#     expect(page).to have_content(item.price)
-#     expect(page).to have_content(cat1.title)
-#     expect(page).to have_content(cat2.title)
-#     expect(page).to have_content(cat2.title)
-#     expect(page.all("img").count).to eq(1)
-#   end
+    expect(page).to have_content(item.name)
+    expect(page).to have_content(item.description)
+    expect(page).to have_content(item.price)
+    expect(page).to have_content(cat1.title)
+    expect(page).to have_content(cat2.title)
+    expect(page).to have_content(cat2.title)
+    expect(page.all("img").count).to eq(2)
+  end
 
 #   xit "cannot create an item without title, description, or price" do
 #     admin = create(:user, role: 1)
