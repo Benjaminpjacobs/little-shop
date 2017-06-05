acdc688fc61523b2772db07636d04dcd0566c9dd
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-05 12:30:06 -0600

    add s3 protocol

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   validates :name, :description, :price, :status, presence: :true
   validates :name, uniqueness: :true
   has_attached_file :image
+              :storage => :s3,
+              # :s3_credentials => {:access_key_id => "AKIAJLT6CCBIYP5HOY6Q", :secret_access_key => "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"},
+              :s3_protocol => :https,
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   has_many :category_items, dependent: :destroy
   has_many :categories, through: :category_items
