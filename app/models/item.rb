8ce25d5781dfcfac58cf673c06b263634e2fe775
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-05 12:05:06 -0600

    adds dependent destroy to order and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   has_many :category_items, dependent: :destroy
   has_many :categories, through: :category_items
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
   enum status: [:available, :retired]
 end
