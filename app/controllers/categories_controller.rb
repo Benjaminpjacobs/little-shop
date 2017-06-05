ab50a2ca9d354032883df45d9ddab52d66963cdb
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-05 13:44:23 -0600

    adds pagination for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @items = @category.items.paginate(:page => params[:page], :per_page => 4)
   end
 
 end
