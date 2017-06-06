d4d435b5c3b34ddd7166e2fda6c65ff40513364b
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-06 14:39:21 -0600

    creates test for adding address to profile from dashboard

--- spec/features/users/registered_user_can_edit_address_from_dashboard_spec.rb
+++ spec/features/users/registered_user_can_edit_address_from_dashboard_spec.rb
@@ -1,28 +1,32 @@
-# require "rails_helper"
-#
-# RSpec.feature "As registered user" do
-#   it "can edit profile" do
-#
-#     user = create(:user_with_address)
-#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-#     visit root_path
-#     click_on ("#{user.first_name}")
-#
-#     expect(current_path).to eq(dashboard_index_path)
-#
-#     expect(page).to have_link("Edit Profile")
-#     click_on "Edit Profile"
-#     expect(current_path).to eq(edit_user_path(user))
-#
-#     expect(page).to have_content("Edit Profile")
-#     expect(page).to have_selector("input[value='#{user.first_name}']")
-#     expect(page).to have_selector("input[value='#{user.last_name}']")
-#     expect(page).to have_selector("input[value='#{user.email}']")
-#
-#     fill_in "First name", with: "Peach"
-#     fill_in "Last name", with: "Cobbler"
-#     fill_in "Email", with: "buttercrust@ilovebutter.com"
-#
-#     click_on "Update Profile"
-#     expect(current_path).to eq(dashboard_index_path)
-#   end
+require "rails_helper"
+
+RSpec.feature "As registered user" do
+  it "can add address to profile" do
+
+    user = create(:user)
+    address = build(:address)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit dashboard_index_path
+
+    click_on "Add Address"
+
+    expect(current_path).to eq(new_users_address_path)
+
+    fill_in "Street Address", with: address.street_address
+    fill_in "City", with: address.city
+    fill_in "State", with: address.state
+    fill_in "Zipcode", with: address.zipcode
+
+    click_button "Add Address"
+
+    expect(current_path).to eq(dashboard_index_path)
+
+    expect(page).to have_content(address.street_address)
+    expect(page).to have_content(address.city)
+    expect(page).to have_content(address.state)
+    expect(page).to have_content(address.zip)
+
+  end
+end
+
