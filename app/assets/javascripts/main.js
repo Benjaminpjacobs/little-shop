973cf38632212c54833cc44d258779cd3e5fcd40
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-06 16:13:24 -0600

    copies code snippets from branch

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,29 @@
-$(document).ready(function(){
-  $(".order-filter a").click(function(){
-   $("#all-orders").find("." + this.id).addClass("show")
-   $("#all-orders").find("." + this.id).removeClass("hidden")
-   $("#all-orders .orders-table").not("." + this.id).removeClass("show")
-   $("#all-orders .orders-table").not("." + this.id).addClass("hidden")
- })
+$(document).ready(function() {
+    $(".order-filter a").click(function() {
+        $("#all-orders").find("." + this.id).addClass("show")
+        $("#all-orders").find("." + this.id).removeClass("hidden")
+        $("#all-orders .orders-table").not("." + this.id).removeClass("show")
+        $("#all-orders .orders-table").not("." + this.id).addClass("hidden")
+    })
 
-  $("#show-all").click(function(){
-    $("#all-orders .orders-table").removeClass("hidden")
-  })
+    $("#show-all").click(function() {
+        $("#all-orders .orders-table").removeClass("hidden")
+    })
 });
+
+
+
+// $(document).ready(function() {
+
+//     $(".order-filter a").click(function() {
+//         $.get('/admin/dashboard/index', this.id, callback, null, "script");
+//         return false;
+//     })
+// });
+
+// $(document).ready(function() {
+//     $("#search_form input").keyup(function() {
+//         $.get($("#search_form").attr("action"), $("#search_form").serialize(), null, "script");
+//         return false;
+//     });
+// });
