be9e945779b0d27947c5cc265f4dfa5a313f1b06
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-06 11:22:17 -0600

    adds before actions in admin controller

--- app/controllers/cart/item_controller.rb
+++ app/controllers/cart/item_controller.rb
@@ -1,5 +1,6 @@
 class Cart::ItemController < ApplicationController
   before_action :set_item
+  
   def update
     @cart.contents[@item.id.to_s] = params[:quantity].to_i
     flash[:updated_cart] = "Successfully updated #{view_context.link_to(@item.name, item_path(@item))} quantity in your cart."
--- app/controllers/users/addresses_controller.rb
+++ app/controllers/users/addresses_controller.rb
@@ -1,4 +1,10 @@
 class Users::AddressesController < ApplicationController
+
+  def new
+    @address = Address.new
+    @current_path = params[:current_path] if params[:current_path]
+  end
+
   def create
     @address = Address.create(address_params)
     @address.user_id = current_user.id
@@ -6,31 +12,14 @@ class Users::AddressesController < ApplicationController
       flash[:success] = "Address was successfully saved"
       redirect_to carts_path
     else
+      render :new
     end
   end
 
-  def new
-    @address = Address.new
-    @current_path = params[:current_path] if params[:current_path]
-  end
 
 private
 
   def address_params
     params.require(:address).permit(:street_address, city_attributes: [:name], state_attributes: [:name], zipcode_attributes:[:number])
   end
-
-  def current_path
-    params.require(:address).permit(:current_path)[:current_path]
-  end
-
-  # def redirect_path
-  #   if current_path == carts_path
-  #     binding.pry
-  #     redirect_to user_order_path(current_user.id, current_user.orders.new), :method => :post
-  #   else
-  #     redirect_to dashboard_index
-  #   end
-  # end
-
 end
