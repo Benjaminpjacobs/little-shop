fcf182bcf1bae65b9ce517ee9494c1539550d7a6
Padraic O'Neill <pconeill@gmail.com [core]>
2017-06-07 11:14:52 -0600

    adds delete item button to admin all items page

--- spec/features/admin/admin_can_delete_items_spec.rb
+++ spec/features/admin/admin_can_delete_items_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "As an admin" do
-  it "can delete an item" do
+  it "can delete an item from admin item edit" do
     admin = create(:user, role: 1)
     item = create(:item)
 
@@ -17,6 +17,24 @@ RSpec.describe "As an admin" do
 
     expect(page).to have_button("Delete Item")
     click_button "Delete Item"
+    expect(page).to have_content("Item Annihilated!")
+    expect(current_path).to eq(admin_items_path)
+  end
+
+  it "can delete an item from admin items page" do
+    admin = create(:user, role: 1)
+    item = create(:item)
+
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    click_on "View All Items"
+    expect(page).to have_link("Delete")
+    click_on "Delete"
+    expect(page).to_not have_content(item.name)
     expect(page).to have_content("Item Annihilated!")
     expect(current_path).to eq(admin_items_path)
   end
